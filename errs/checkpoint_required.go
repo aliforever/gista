344aa6b75c43e84e0f1c37a16065e5723b3a78d7
aliforever <alipadida@live.com>
2019-06-01 02:15:18 +0430

    - Changed CheckPoint Error

--- errs/checkpoint_required.go
+++ errs/checkpoint_required.go
@@ -1,11 +1,25 @@
 package errs
 
-type CheckpointRequired GistaError
+import (
+	"github.com/aliforever/gista/responses"
+)
 
-func (ck CheckpointRequired) Error() string {
+type CheckpointRequired struct {
+	Type         *string
+	Message      *string
+	HTTPResponse responses.ResponseInterface
+}
+
+func (cp CheckpointRequired) Error() string {
 	m := "unknown"
-	if ck.Message != nil {
-		m = *ck.Message
+	if cp.Message != nil {
+		m = *cp.Message
 	}
 	return m
+}
+
+func (cp CheckpointRequired) GetCheckpointUrl() string {
+	obj := cp.HTTPResponse.(interface{})
+	checkpointUrl := obj.(*responses.Login).CheckPointUrl
+	return checkpointUrl
 }
