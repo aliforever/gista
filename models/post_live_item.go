48ee43f3484fb4b8b041b4db356338a0aea7df51
aliforever <alipadida@live.com>
2019-06-03 02:26:02 +0430

    - Changes

--- models/post_live_item.go
+++ models/post_live_item.go
@@ -1,14 +1,14 @@
 package models
 
 type PostLiveItem struct {
-	Pk                  string      `json:"pk"`
-	User                User        `json:"user"`
-	Broadcasts          []Broadcast `json:"broadcasts"`
-	PeakViewerCount     int         `json:"peak_viewer_count"`
-	LastSeenBroadcastTs interface{} `json:"last_seen_broadcast_ts"`
-	CanReply            interface{} `json:"can_reply"`
-	RankedPosition      interface{} `json:"ranked_position"`
-	SeenRankedPosition  int         `json:"seen_ranked_position"`
-	Muted               interface{} `json:"muted"`
-	CanReshare          interface{} `json:"can_reshare"`
+	Pk                  string       `json:"pk"`
+	User                User         `json:"user"`
+	Broadcasts          *[]Broadcast `json:"broadcasts"`
+	PeakViewerCount     int          `json:"peak_viewer_count"`
+	LastSeenBroadcastTs interface{}  `json:"last_seen_broadcast_ts"`
+	CanReply            interface{}  `json:"can_reply"`
+	RankedPosition      interface{}  `json:"ranked_position"`
+	SeenRankedPosition  int          `json:"seen_ranked_position"`
+	Muted               interface{}  `json:"muted"`
+	CanReshare          interface{}  `json:"can_reshare"`
 }
